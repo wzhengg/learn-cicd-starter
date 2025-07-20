a9be56d3c3da390ff290e226edea9313c51afe50
William Zheng <williamzheng2001@gmail.com>
2025-07-20 18:57:36 -0400

    broken test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := http.Header{
+		"uthorization": []string{"ApiKey api-key"},
+	}
+
+	apiKey, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("err: %s", err.Error())
+	}
+
+	if apiKey != "api-key" {
+		t.Fatalf("expected: %s, got %s", "api-key", apiKey)
+	}
+}
